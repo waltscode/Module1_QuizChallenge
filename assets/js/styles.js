3f70fbd3468c63520c9d622c1b6e177ebd7105bc
waltscode <walterswork91@outlook.com>
2023-10-29 17:37:27 -0400

    added submit form button, play again button, changed the css of highscore page, enabled local storage of highscores

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -51,25 +51,31 @@ const quizContainer = document.getElementById('quiz-container');
 const timerElement = document.getElementById('timer');
 
 let timeRemaining = 40;
-
+let countdown;
 // adding an eventlistener to the START BUTTON in order to launch the quiz and remove the display:none setting on the quiz - MAKING IT VISIBLE
 startButton.addEventListener('click', () => {
     startContainer.style.display = 'none';
     quizContainer.style.display = 'block';
     timerElement.style.display = 'block';
+    resetTimer();
     const countdown = setInterval(function () {
         timeRemaining--;
         timerElement.textContent = `Timer: ${timeRemaining}`;
-        if (timeRemaining <= 0) {
+        if (timeRemaining <= 0 || currentQuestionIndex[3]) {
             clearInterval(countdown);
             alert('Oops! Out of Time!');
-            showScore();
+            showScore()
         }
-    },1000);
+
+    }, 1000);
     startQuiz();
 });
 
-
+function resetTimer() {
+    clearInterval(countdown);
+    timeRemaining = 40;
+    timerElement.textContent = `Timer: ${timeRemaining}`;
+}
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -78,7 +84,8 @@ function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
-       showQuestion();
+    showQuestion();
+    resetTimer();
 }
 
 
@@ -129,30 +136,118 @@ function selectAnswer(e) {
     nextButton.style.display = "block";
 }
 
-function showScore(){
+function showScore() {
     resetState();
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
-    nextButton.innerHTML = "Play Again";
-    nextButton.style.display = 'block'; 
+    
+    const playAgainButton = document.createElement("button");
+    playAgainButton.innerHTML = "Play Again";
+    playAgainButton.classList.add('btn', 'play-again-btn');
+    answerButtons.appendChild(playAgainButton);
+
+    const submitScoreButton = document.createElement("button");
+    submitScoreButton.innerHTML = "Submit Score";
+    submitScoreButton.classList.add('btn', 'submit-score-btn');
+    answerButtons.appendChild(submitScoreButton);
+
+    submitScoreButton.addEventListener('click', () => {
+        const scoreForm = document.getElementById('score-form');
+        scoreForm.style.display = 'block';
+        playAgainButton.style.display = 'none';
+        submitScoreButton.style.display = 'none';
+    });
+
+    playAgainButton.addEventListener('click', () => {
+        startQuiz();
+
+    });
+
+    playAgainButton.style.display = 'block';
+    submitScoreButton.style.display = 'block';
 }
 
 
-function handleNextButton(){
+const scoreForm = document.getElementById('score-form');
+
+scoreForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    saveHighScore(username, score);
+    alert('Score submitted successfully!');
+    startQuiz();
+});
+
+
+function saveHighScore(username, score) {
+    const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+    highScores.push({ username, score });
+    highScores.sort((a, b) => b.score - a.score);
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+}
+
+
+const highscoreButton = document.getElementById('highscoreButton');
+const highscoresContainer = document.getElementById('highscores-container');
+const highscoresList = document.getElementById('highscores-list');
+const closeHighscoresButton = document.getElementById('close-highscores');
+
+highscoreButton.addEventListener('click', showHighscores);
+closeHighscoresButton.addEventListener('click', hideHighscores);
+
+function showHighscores() {
+    const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+    highscoresList.innerHTML = '';
+
+    highScores.forEach((score, index) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${index + 1}. ${score.username}: ${score.score}`;
+        highscoresList.appendChild(listItem);
+    });
+
+
+    highscoresContainer.style.display = 'block';
+    highscoreButton.style.display = "none";
+    timerElement.style.display = "none";
+    quizContainer.style.display = "none";
+    startContainer.style.display = "none";
+}
+
+function hideHighscores() {
+    highscoresContainer.style.display = 'none';
+}
+
+
+function handleNextButton() {
     currentQuestionIndex++;
-    if(currentQuestionIndex < questions.length){
+    if (currentQuestionIndex < questions.length) {
         showQuestion();
-    }else{
+        nextButton.innerHTML = "Next";
+    } else {
         showScore();
+        nextButton.innerHTML = "Test button for End"
+        timeRemaining = 40;
     }
 }
 
-nextButton.addEventListener('click', ()=>{
+nextButton.addEventListener('click', () => {
     if (currentQuestionIndex < questions.length) {
         handleNextButton();
-    }else{
-      startQuiz();  
+    } else {
+        showScore();
+        nextButton.innerHTML = 'test button 12121';
+        resetTimer();
+        resetQuiz();
     }
 });
+
+function resetQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    startContainer.style.display = 'block';
+    quizContainer.style.display = 'none';
+    timerElement.style.display = 'none';
+    nextButton.innerHTML = 'Next';
+}
 
 
 startQuiz();
